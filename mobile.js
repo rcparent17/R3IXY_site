6dd424e32fc3d7476959600910d9c9a65be7e119
Reilly Parent <reilly.c.parent@gmail.com>
2020-02-15 20:05:42 -0500

    function-ized, rearranged, egged ;)

--- mobile.js
+++ mobile.js
@@ -1,44 +1,74 @@
-var elements = document.getElementById('links');
+/*                r
+*    %   %%%%%  %
+*  c    %%     %%
+*   % % |     | % %
+*    %  |  O  |  %          OPKL HKKGEJC WP IU YKZA UKQ ZEJCQO
+*   % % |     | % %
+*      %%     %%  
+*     %  %%%%%  %
+*                z
+*/
+
+//all divs
+var music = document.getElementById('musiclinks');
+//var social = document.getElementById('sociallinks');
+//var support = document.getElementById('supportlinks');
+
+//body reference for site-putting-together-purposes
 var bodyRef = document.getElementById('body');
-
-var logo = document.getElementById('logo');
-logo.style.display = "block";
-logo.style.marginLeft = "auto";
-logo.style.marginRight = "auto";
-
-
-var listen = document.getElementById('listentext');
-listen.style.display = "block";
-listen.style.marginLeft = "auto";
-listen.style.marginRight = "auto";
-
-var links = elements.querySelectorAll('a');
-var images = elements.querySelectorAll('img');
 
 var onMobile = window.innerHeight > window.innerWidth;
 
-if (onMobile) {
-  logo.style.width = "70%";
-}
-else{
-  logo.style.width = "30%";
-  listen.style.fontSize = "2vw";
-}
+initTop();
 
-for (var i = 0; i < links.length; i++) {
-  if (onMobile) {
-    bodyRef.appendChild(links[i]);
-    bodyRef.appendChild(document.createElement("br"));
-    bodyRef.appendChild(document.createElement("br"));
-    bodyRef.appendChild(document.createElement("br"));
-  }
-  else {
-    images[i].style.width = "15%";
-    bodyRef.appendChild(links[i]);
-    if (i==2){
+var mLinks = music.querySelectorAll('a');
+
+
+var mImages = music.querySelectorAll('img');
+
+
+drawGroup(mLinks, mImages, 3);
+
+function drawGroup(links, images, width){
+  for (var i = 0; i < links.length; i++) {
+    if (onMobile) {
+      bodyRef.appendChild(links[i]);
       bodyRef.appendChild(document.createElement("br"));
       bodyRef.appendChild(document.createElement("br"));
       bodyRef.appendChild(document.createElement("br"));
     }
+    else {
+      images[i].style.width = "15%";
+      bodyRef.appendChild(links[i]);
+      if (i === width-1 && width!=-1){
+        bodyRef.appendChild(document.createElement("br"));
+        bodyRef.appendChild(document.createElement("br"));
+        bodyRef.appendChild(document.createElement("br"));
+      }
+    }
   }
+}
+
+function initTop(){
+  var logo = document.getElementById('logo');
+  logo.style.display = "block";
+  logo.style.marginLeft = "auto";
+  logo.style.marginRight = "auto";
+
+  var listen = document.getElementById('listentext');
+  listen.style.display = "block";
+  listen.style.marginLeft = "auto";
+  listen.style.marginRight = "auto";
+
+  if (onMobile) {
+    logo.style.width = "70%";
+  } 
+  else {
+    logo.style.width = "30%";
+    listen.style.fontSize = "4vw";
+  }
+}
+
+function bigBreak(){
+
 }
