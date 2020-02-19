fd5482a365a2260bea3fb2d8471f663f563af642
Reilly Parent <rparen16@ford.com>
2020-02-18 20:47:14 -0500

    added follow section

--- mobile.js
+++ mobile.js
@@ -4,14 +4,14 @@
 *   % % |     | % %
 *    %  |  O  |  %          OPKL HKKGEJC WP IU YKZA UKQ ZEJCQO
 *   % % |     | % %
-*      %%     %%  
+*      %%     %%
 *     %  %%%%%  %
 *                z
 */
 
 //all divs
 var music = document.getElementById('musiclinks');
-//var social = document.getElementById('sociallinks');
+var social = document.getElementById('sociallinks');
 //var support = document.getElementById('supportlinks');
 
 //body reference for site-putting-together-purposes
@@ -22,53 +22,66 @@ var onMobile = window.innerHeight > window.innerWidth;
 initTop();
 
 var mLinks = music.querySelectorAll('a');
-
+var socLinks = social.querySelectorAll('a');
 
 var mImages = music.querySelectorAll('img');
+var socImages = social.querySelectorAll('img');
 
+drawGroup(mLinks, mImages, 3, "15%");
 
-drawGroup(mLinks, mImages, 3);
+follow = initElement("followtext");
+if(onMobile){
+  follow.style.fontSize = "7vw";
+}
+else {
+  follow.style.fontSize = "4vw";
+}
 
-function drawGroup(links, images, width){
+bodyRef.appendChild(follow);
+
+drawGroup(socLinks, socImages, 3, "10%");
+
+function drawGroup(links, images, width, imgSize){
   for (var i = 0; i < links.length; i++) {
     if (onMobile) {
       bodyRef.appendChild(links[i]);
-      bodyRef.appendChild(document.createElement("br"));
-      bodyRef.appendChild(document.createElement("br"));
-      bodyRef.appendChild(document.createElement("br"));
+      bigBreak();
     }
     else {
-      images[i].style.width = "15%";
+      images[i].style.width = imgSize;
       bodyRef.appendChild(links[i]);
       if (i === width-1 && width!=-1){
-        bodyRef.appendChild(document.createElement("br"));
-        bodyRef.appendChild(document.createElement("br"));
-        bodyRef.appendChild(document.createElement("br"));
+        bigBreak();
       }
     }
   }
+  bigBreak();
 }
 
 function initTop(){
-  var logo = document.getElementById('logo');
-  logo.style.display = "block";
-  logo.style.marginLeft = "auto";
-  logo.style.marginRight = "auto";
-
-  var listen = document.getElementById('listentext');
-  listen.style.display = "block";
-  listen.style.marginLeft = "auto";
-  listen.style.marginRight = "auto";
+  var logo = initElement("logo");
+  var listen = initElement("listentext");
 
   if (onMobile) {
     logo.style.width = "70%";
-  } 
+    listen.style.fontSize = "7vw";
+  }
   else {
     logo.style.width = "30%";
     listen.style.fontSize = "4vw";
   }
 }
 
-function bigBreak(){
+function initElement(_el) {
+  var el = document.getElementById(_el);
+  el.style.display = "block";
+  el.style.marginLeft = "auto";
+  el.style.marginRight = "auto";
+  return el;
+}
 
+function bigBreak(){
+  bodyRef.appendChild(document.createElement("br"));
+  bodyRef.appendChild(document.createElement("br"));
+  bodyRef.appendChild(document.createElement("br"));
 }
